6cc910246eee8a87bfa560c65a344e8f2f7b0c83
Robin Jütte <juetterobin@gmail.com>
2026-05-29 18:48:51 +0200

    UI

---
+++ RaceTimerApp/RaceTimerApp.Shared/Helpers/TimeSpanHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RaceTimerApp.Shared.Helpers
+{
+    public static class TimeSpanHelper
+    {
+        public static string FormatShort(this TimeSpan? timeSpan)
+        {
+            
+            if (timeSpan == null)
+            {
+                return " - ";
+            }
+
+            return timeSpan.Value.FormatShort();
+        }
+        public static string FormatShort(this TimeSpan timeSpan)
+        {
+            string format = @"mm\:ss";
+            if (timeSpan.TotalHours > 1)
+            {
+                format = @"hh\:" + format;
+
+                if (timeSpan.TotalDays > 1)
+                {
+                    format = @"d\." + format;
+                }
+            }
+
+            return timeSpan.ToString(format);
+            
+        }
+    }
+}
